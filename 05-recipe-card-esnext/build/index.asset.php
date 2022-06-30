a772107adc2a9e31c01e820e9d7cece83cfb9ec3
Leslie Orellana <lesore0789@gmail.com>
2022-06-30 13:54:44 -0500

    Modify H2 tags to H1 tags for recipe title

--- 05-recipe-card-esnext/build/index.asset.php
+++ 05-recipe-card-esnext/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '75e25d09c070839581800a6c1444fa1a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'dcc1d1ab5ed397685c9e399d57b3267a');
